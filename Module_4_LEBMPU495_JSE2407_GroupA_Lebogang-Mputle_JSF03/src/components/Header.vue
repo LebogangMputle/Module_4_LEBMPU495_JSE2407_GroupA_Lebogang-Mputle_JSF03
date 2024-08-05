fca350e27a749b19ae2f0ef416099235ad4b6813
LebogangMputle <lebozikalala@gmail.com>
2024-08-05 10:48:45 +0200

    imported index.css to my main.js and configured tailwind

--- Module_4_LEBMPU495_JSE2407_GroupA_Lebogang-Mputle_JSF03/src/components/Header.vue
+++ Module_4_LEBMPU495_JSE2407_GroupA_Lebogang-Mputle_JSF03/src/components/Header.vue
@@ -15,7 +15,6 @@
           <li><a href="#" class="text-white md:hover:text-blue-700">Wishlist</a></li>
           <li class="relative hidden md:block">
             <div class="absolute left-3 top-0">
-              <p class="bg-red-500 text-white text-xs p-1 rounded-full">2</p>
             </div>
             <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
@@ -38,3 +37,7 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+/* Add any additional styles you need here */
+</style>
--- Module_4_LEBMPU495_JSE2407_GroupA_Lebogang-Mputle_JSF03/src/main.js
+++ Module_4_LEBMPU495_JSE2407_GroupA_Lebogang-Mputle_JSF03/src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './store/index';
+import router from './router/index';
 import store from './store/index'; // import Vuex store
+import './assets/index.css'; // Ensure this path is correct
+
+
+// Polyfill for process
+import process from 'process';
+window.process = process;
 
 createApp(App)
   .use(router)
