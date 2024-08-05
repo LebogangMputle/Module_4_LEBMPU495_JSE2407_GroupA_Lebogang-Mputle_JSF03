7617d45baa382be595961860b0e6f8837a724ca4
LebogangMputle <lebozikalala@gmail.com>
2024-08-05 09:17:17 +0200

    Created the header and productlist component

--- Module_4_LEBMPU495_JSE2407_GroupA_Lebogang-Mputle_JSF03/src/App.vue
+++ Module_4_LEBMPU495_JSE2407_GroupA_Lebogang-Mputle_JSF03/src/App.vue
@@ -1,30 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <Header @toggle-login="showLoginModal = !showLoginModal" />
+    <ProductList v-if="isLoggedIn" @select-product="selectedProduct = $event" />
+    <ProductDetails v-if="selectedProduct" :product="selectedProduct" @back="selectedProduct = null" />
+    <LoginModal v-if="showLoginModal" @login="handleLogin" @cancel="showLoginModal = false" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import Header from './components/Header.vue';
+import ProductList from './components/ProductList.vue';
+import ProductDetails from './components/ProductDetails.vue';
+import LoginModal from './components/LoginModal.vue';
+
+export default {
+  components: {
+    Header,
+    ProductList,
+    ProductDetails,
+    LoginModal
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+      showLoginModal: true,
+      selectedProduct: null
+    };
+  },
+  methods: {
+    handleLogin() {
+      this.isLoggedIn = true;
+      this.showLoginModal = false;
+    }
+  }
+};
+</script>
