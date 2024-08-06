0b74d06ff829c4b59e0306681508037189027324
LebogangMputle <lebozikalala@gmail.com>
2024-08-06 13:32:59 +0200

    "Refactored Vuex store code to separate state, mutations, actions, and getters into individual objects, added JSDoc-style comments for documentation."

--- Module_4_LEBMPU495_JSE2407_GroupA_Lebogang-Mputle_JSF03/src/store/index.js
+++ Module_4_LEBMPU495_JSE2407_GroupA_Lebogang-Mputle_JSF03/src/store/index.js
@@ -1,45 +1,141 @@
+/**
+ * @module store
+ */
+
 import { createStore } from 'vuex';
 
-const store = createStore({
-  state: {
-    products: [],
-    cart: [],
-    wishlist: [],
-    isLoggedIn: false
-  },
-  mutations: {
-    setProducts(state, products) {
-      state.products = products;
-    },
-    addToCart(state, product) {
-      state.cart.push(product);
-    },
-    removeFromCart(state, productId) {
-      state.cart = state.cart.filter(product => product.id !== productId);
-    },
-    addToWishlist(state, product) {
-      state.wishlist.push(product);
-    },
-    removeFromWishlist(state, productId) {
-      state.wishlist = state.wishlist.filter(product => product.id !== productId);
-    },
-    setIsLoggedIn(state, status) {
-      state.isLoggedIn = status;
-    }
-  },
-  actions: {
-    async fetchProducts({ commit }) {
-      const response = await fetch('https://fakestoreapi.com/products');
-      const products = await response.json();
-      commit('setProducts', products);
-    }
-  },
-  getters: {
-    products: (state) => state.products,
-    cart: (state) => state.cart,
-    wishlist: (state) => state.wishlist,
-    isLoggedIn: (state) => state.isLoggedIn
-  }
-});
+/**
+ * @typedef {Object} State
+ * @property {Array} products - List of products.
+ * @property {Array} cart - List of products in the cart.
+ * @property {Array} wishlist - List of products in the wishlist.
+ * @property {boolean} isLoggedIn - User's login status.
+ */
 
-export default store;
+/**
+ * State object for the Vuex store.
+ * @type {State}
+ */
+const state = {
+  products: [],
+  cart: [],
+  wishlist: [],
+  isLoggedIn: false,
+};
+
+/**
+ * Mutations for the Vuex store.
+ * @type {Object}
+ */
+const mutations = {
+  /**
+   * Sets the products state.
+   * @param {State} state - The state object.
+   * @param {Array} products - The list of products.
+   */
+  setProducts(state, products) {
+    state.products = products;
+  },
+
+  /**
+   * Adds a product to the cart.
+   * @param {State} state - The state object.
+   * @param {Object} product - The product to add.
+   */
+  addToCart(state, product) {
+    state.cart.push(product);
+  },
+
+  /**
+   * Removes a product from the cart.
+   * @param {State} state - The state object.
+   * @param {number} productId - The ID of the product to remove.
+   */
+  removeFromCart(state, productId) {
+    state.cart = state.cart.filter(product => product.id !== productId);
+  },
+
+  /**
+   * Adds a product to the wishlist.
+   * @param {State} state - The state object.
+   * @param {Object} product - The product to add.
+   */
+  addToWishlist(state, product) {
+    state.wishlist.push(product);
+  },
+
+  /**
+   * Removes a product from the wishlist.
+   * @param {State} state - The state object.
+   * @param {number} productId - The ID of the product to remove.
+   */
+  removeFromWishlist(state, productId) {
+    state.wishlist = state.wishlist.filter(product => product.id !== productId);
+  },
+
+  /**
+   * Sets the user's login status.
+   * @param {State} state - The state object.
+   * @param {boolean} status - The login status.
+   */
+  setIsLoggedIn(state, status) {
+    state.isLoggedIn = status;
+  },
+};
+
+/**
+ * Actions for the Vuex store.
+ * @type {Object}
+ */
+const actions = {
+  /**
+   * Fetches the products from the API and commits the setProducts mutation.
+   * @param {Object} context - The Vuex context object.
+   */
+  async fetchProducts({ commit }) {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    commit('setProducts', data);
+  },
+};
+
+/**
+ * Getters for the Vuex store.
+ * @type {Object}
+ */
+const getters = {
+  /**
+   * Retrieves the list of products.
+   * @param {State} state - The state object.
+   * @returns {Array} The list of products.
+   */
+  products: (state) => state.products,
+
+  /**
+   * Retrieves the list of products in the cart.
+   * @param {State} state - The state object.
+   * @returns {Array} The list of products in the cart.
+   */
+  cart: (state) => state.cart,
+
+  /**
+   * Retrieves the list of products in the wishlist.
+   * @param {State} state - The state object.
+   * @returns {Array} The list of products in the wishlist.
+   */
+  wishlist: (state) => state.wishlist,
+
+  /**
+   * Retrieves the user's login status.
+   * @param {State} state - The state object.
+   * @returns {boolean} The user's login status.
+   */
+  isLoggedIn: (state) => state.isLoggedIn,
+};
+
+export default createStore({
+  state,
+  mutations,
+  actions,
+  getters,
+});
